e58637fd48902720ff5881364d6f4d2b4643b4ac
kian maleki <150921250+kianmaleki@users.noreply.github.com>
2024-05-19 20:00:54 +0330

    add

--- mahsol-page.php
+++ mahsol-page.php
@@ -1,35 +1,15 @@
 <!DOCTYPE html>
-<html lang="fa">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>نام محصول</title>
     <link rel="stylesheet" href="style.css">
+    <title>mahsol</title>
 </head>
 
 <body>
-    <header>
-        <h1>نام محصول</h1>
-    </header>
 
-    <main>
-        <section class="product-image">
-            <img src="image.jpg" alt="نام محصول">
-        </section>
-
-        <section class="product-info">
-            <h2>اطلاعات محصول</h2>
-            <ul>
-                <li>**قیمت:** قیمت محصول</li>
-                <li>**توضیحات:** توضیحات مختصری درباره محصول</li>
-            </ul>
-
-            <button class="add-to-cart">افزودن به سبد خرید</button>
-        </section>
-    </main>
-
-    <script src="script.js"></script>
 </body>
 
 </html>
