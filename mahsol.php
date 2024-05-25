163bb6ec0881a3971d1cd4ac1d9d14bd0b0fd1e6
kian maleki <150921250+kianmaleki@users.noreply.github.com>
2024-05-25 22:59:40 +0330

    add

--- mahsol.php
+++ mahsol.php
@@ -78,14 +78,14 @@ $result = mysqli_query($connect, $sql);
         }
 
         .product-info-price .price {
-            color: green;
+            color: rgb(0, 255, 0);
             padding: 0.5rem;
             margin: 0.5rem;
             font-size: 1.5rem;
         }
 
         .product-info-price .off-price {
-            color: red;
+            color: rgb(255, 0, 0);
             text-decoration: line-through;
             padding: 0.5rem;
             margin: 0.5rem;
