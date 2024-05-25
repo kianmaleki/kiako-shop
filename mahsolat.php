1ecb934ad761145920c41764de56304ec52d1e74
kian maleki <150921250+kianmaleki@users.noreply.github.com>
2024-05-25 19:43:01 +0330

    add

--- mahsolat.php
+++ mahsolat.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <!-- Latest compiled JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="style.css">
 
     <title>نوشیدنی ها</title>
 </head>
